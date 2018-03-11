9058e31a55704e362b1b1d206a26d32856ef16ed
ubuntu <ubuntu@localhost.localdomain>
2018-03-11 20:00:47 +0800

    p1

--- homework1/helloworld/main.cc
+++ homework1/helloworld/main.cc
@@ -1,8 +1,13 @@
 // Copyright @2018 Pony AI Inc. All rights reserved.
 //
 #include <iostream>
+#include <string>
 
 int main() {
   std::cout << "Hello World." << std::endl;
+  int a,b; 
+  a = 1;
+  b = 2;
+  std::cout << a + b << std::endl;
   return 0;
 }
