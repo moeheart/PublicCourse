9149d7c0776fb573db894866a1428e9cf9cb3435
moeheart <957685908@qq.com>
2018-03-31 14:21:37 +0800

    h2p1 try output

---
+++ homework2/output.cc
@@ -0,0 +1,28 @@
+// Copyright @2018 Pony AI Inc. All rights reserved.
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+#include "homework2/pointcloud.h"
+
+int main() {
+  // ATTENTION!!! : please use absolute path for reading the data file.
+  const PointCloud pointcloud = ReadPointCloudFromTextFile(
+      "/home/ubuntu/PublicCourse/homework2/sample_data/VelodyneDevice32c/0.txt");
+  std::cout << "Total points read: " << pointcloud.points.size() << std::endl;
+  std::cout << "Rotation: " << std::endl;
+  std::cout << pointcloud.rotation << std::endl;
+  std::cout << "Translation: " << std::endl;
+  std::cout << pointcloud.translation.transpose() << std::endl;
+  ofstream fout("p1.txt");
+  fout<<"Range"<<endl;
+  for p in pointCloud.points{
+      fout<<setiosflag(ios::fixed)<<setprecision(6)<<sqrt(p(0)*p(0)+p(1)*p(1)+p(2)*p(2))<<endl;
+  }
+  fout<<"Height"<<endl;
+  for p in pointCloud.points{
+      fout<<setiosflag(ios::fixed)<<setprecision(6)<<p(2)<<endl;
+  }
+  return 0;
+}
